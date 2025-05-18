74e9a188e7c3fb9b9e4f303cb1529e5f3d1271ac
gleuzoi <guelzoi@outlook.com>
2025-05-17 17:20:33 -0700

    feat: UserModel label field to be static

--- App/Models/UserModel.cs
+++ App/Models/UserModel.cs
@@ -3,7 +3,7 @@ namespace SPAL.App.Models;
 
 public class UserModel : ITableModel
 {
-    public string Label => "User";
+    public static string Label => "User";
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Email { get; set; }
