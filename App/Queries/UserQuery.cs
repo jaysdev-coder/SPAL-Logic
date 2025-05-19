e4cc63ef1b5de56131184e9409c2d2b188c31de5
gleuzoi <guelzoi@outlook.com>
2025-05-19 00:14:04 -0700

    feat: add userQuery which inherits ISqlQueryT

---
+++ App/Queries/UserQuery.cs
@@ -0,0 +1,21 @@
+﻿using AWRD.Queries;
+using SPAL.App.Models;
+using SqlKata;
+
+namespace SPAL.App.Queries
+{
+    public class UserQuery : ISqlQueryT<UserModel>
+    {
+        public override string EntityName => UserModel.Label;
+
+        public override Query BuildBaseQuery()
+        {
+            return new Query(EntityName);
+        }
+
+        public override Query BuildCustomQuery(Query query)
+        {
+            return query;
+        }
+    }
+}
