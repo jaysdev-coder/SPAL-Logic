ce74149554d487e8b2e69fcce69c066d76dd9aa4
gleuzoi <guelzoi@outlook.com>
2025-05-26 19:57:45 -0700

    feat: add name and email filter on user query

--- App/Queries/UserQuery.cs
+++ App/Queries/UserQuery.cs
@@ -8,13 +8,35 @@ namespace SPAL.App.Queries
     {
         public override string EntityName => UserModel.Label;
 
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
         public override Query BuildBaseQuery()
         {
             return new Query(EntityName);
         }
 
+        private void BuildNameQuery(Query query)
+        {
+            if(!string.IsNullOrEmpty(Name))
+            {
+                query.Where(nameof(UserModel.Name), "=", Name);
+            }
+        }
+
+        private void BuildEmailQuery(Query query)
+        {
+            if (!string.IsNullOrEmpty(Email))
+            {
+                query.Where(nameof(UserModel.Email), "=", Email);
+            }
+        }
+
         public override Query BuildCustomQuery(Query query)
         {
+            BuildNameQuery(query);
+            BuildEmailQuery(query);
+
             return query;
         }
     }
