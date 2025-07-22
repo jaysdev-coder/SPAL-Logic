9f72df1726527bbc8e54cc843a1700e396c539c5
gleuzoi <guelzoi@outlook.com>
2025-07-22 15:22:28 -0700

    feat: add password hasher

--- App/Models/UserWriteModel.cs
+++ App/Models/UserWriteModel.cs
@@ -4,5 +4,7 @@
     {
         public string? Name { get; set; }
         public string? Email { get; set; }
+        public string? PasswordHash { get; }
+        public string? Salt { get; set; }
     }
 }
