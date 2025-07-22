8b0bd635706f0c1782e9100b7ef0bb9d07691d9e
gleuzoi <guelzoi@outlook.com>
2025-07-22 14:43:35 -0700

    feat: make userGuid required on userReadModel

--- App/Models/UserReadModel.cs
+++ App/Models/UserReadModel.cs
@@ -2,7 +2,7 @@
 {
     public class UserReadModel
     {
-        public Guid UserGuid { get; set; }
+        public required Guid UserGuid { get; set; }
         public required string Name { get; set; }
         public required string Email { get; set; }
     }
